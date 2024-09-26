d9a36750439a1a8c7a058a680ec2f79077800dd5
Md. Mahamud Mredha <mahamud15-13756@diu.edu.bd>
2024-09-26 16:29:20 +0600

    added 2 features

---
+++ app/src/main/java/com/example/vumijorip/Fita_Calculator.java
@@ -0,0 +1,14 @@
+package com.example.vumijorip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fita_Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fita_calculator);
+    }
+}
